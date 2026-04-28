c7bcbb5913d48210deab1d3219d5424c4efaa49b
douglassaoficial-debug <douglassaoficial@gmail.com>
2026-04-28 10:56:13 -0300

    Modify settings for production deployment
    Updated Django settings for production environment.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dif!+v+=bectho$x)dbsu#sb*#^z=$qr*848ckh$80vu00tdpb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
